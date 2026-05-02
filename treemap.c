b5a2b9c104fe906474df8df7b11982dc1c99e400
Your Name <you@example.com>
2026-05-02 02:47:58 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -135,6 +135,7 @@ TreeNode * minimum(TreeNode * x){
 // Reemplace los datos (key,value) de node con los del nodo "minimum". Elimine el nodo minimum (para hacerlo puede usar la misma función removeNode).
 
 void removeNode(TreeMap * tree, TreeNode* node) {
+    if(node == NULL) return;
     TreeNode*hijo = NULL;
     TreeNode*abuelo = node->parent;
     if(node->left == NULL || node->right == NULL){
